2754afe9128cb575b18cca23085e62453421e171
Belhal Karimi <belhal.karimi@gmail.com>
2024-05-19 10:34:28 +0200

    llm openai

--- src/taitriscore/basebot/openai_api.py
+++ src/taitriscore/basebot/openai_api.py
@@ -140,25 +140,27 @@ class OpenAIGPTAPI(BaseGPTAPI, RateLimiter):
     def _call_chat(self, messages):
         # tmp_input = self._cons_kwargs(messages)
         # res = self.llm.ChatCompletion.create(**tmp_input)
+
+        res = self.llm.ChatCompletion.create(**self._cons_kwargs(messages))
         
-        res = {"choices": [
-                    {
-                    "index": 0,
-                    "message": {
-                        "role": "assistant",
-                        "content": "all good."
-                    },
-                    "logprobs": 'null',
-                    "finish_reason": "stop"
-                    }
-                ],
-                "usage": {
-                    "prompt_tokens": 102,
-                    "completion_tokens": 389,
-                    "total_tokens": 491
-                },
-                "system_fingerprint": 'null'
-                }
+        # res = {"choices": [
+        #             {
+        #             "index": 0,
+        #             "message": {
+        #                 "role": "assistant",
+        #                 "content": "all good."
+        #             },
+        #             "logprobs": 'null',
+        #             "finish_reason": "stop"
+        #             }
+        #         ],
+        #         "usage": {
+        #             "prompt_tokens": 102,
+        #             "completion_tokens": 389,
+        #             "total_tokens": 491
+        #         },
+        #         "system_fingerprint": 'null'
+        #         }
         return res
 
     def ask(self, prompt="Can you tell me what you know about Influencers Marketing?"):
